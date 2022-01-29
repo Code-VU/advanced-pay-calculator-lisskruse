697c6c825a4c84188a37db74c0a74d013e72e59d
Melissa Kruse <Melissa.Kruse@vu.com>
2022-01-29 16:49:28 -0600

    correct float conversion

--- payCalculator.py
+++ payCalculator.py
@@ -1,16 +1,16 @@
 def calculatePay():  
     # This first line is provided for you
     hrs = input("Enter Hours: ")
-    hrs = float(hrs)
+    floatHrs = float(hrs)
     rate = input("Enter Rate: ")
-    rate = float(rate)
-    if hrs > 40:
-        overTimeHrs = hrs - 40
+    floatRate = float(rate)
+    if floatHrs > 40:
+        overTimeHrs = floatHrs - 40
         regularHrs = 40
     else:
         overTimeHrs = 0
-        regularHrs = hrs
-    pay = regularHrs * rate + overTimeHrs * rate * 1.5
+        regularHrs = floatHrs
+    pay = regularHrs * floatRate + overTimeHrs * floatRate * 1.5
     print("Pay:", pay)
     # end assignment
 
