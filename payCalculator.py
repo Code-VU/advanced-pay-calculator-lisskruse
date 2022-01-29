139fc7630ddadaa8b8d2da029261ffe46b9445c1
Melissa Kruse <Melissa.Kruse@vu.com>
2022-01-29 16:40:47 -0600

    initial code

--- payCalculator.py
+++ payCalculator.py
@@ -1,8 +1,17 @@
-def calculatePay():
-    
+def calculatePay():  
     # This first line is provided for you
-    hrs = input("Enter Hours:")
-    
+    hrs = input("Enter Hours: ")
+    hrs = float(hrs)
+    rate = input("Enter Rate: ")
+    rate = float(rate)
+    if hrs > 40:
+        overTimeHrs = hrs - 40
+        regularHrs = 40
+    else:
+        overTimeHrs = 0
+        regularHrs = hrs
+    pay = regularHrs * rate + overTimeHrs * rate * 1.5
+    print("Pay:", pay)
     # end assignment
 
 ## if you want to test locally before you try to sync
